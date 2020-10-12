343f6a5d37635b67440c8954c8e9ae30d11db603
Konstantin Kurganov <kurganovk@gmail.com>
2020-10-12 18:44:42 +0300

    Change pdfjs-dist to react-pdf
    pdfjs-dist is one of dependencies of react-pdf

--- src/views/PDFViewer.tsx
+++ src/views/PDFViewer.tsx
@@ -1,39 +1,18 @@
 import * as React from 'react';
-
-import * as pdfjsLib from 'pdfjs-dist';
+import { Document, Page } from 'react-pdf';
 
 const pdfPath = '/public/example.pdf';
 
-export default class PDFViewer extends React.Component {
-  componentDidMount() {
-    // TODO FIX FAKE WORKER
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '../../pdf.worker.js';
+export interface IPDFViewerProps {}
 
-    const loadingTask = pdfjsLib.getDocument(pdfPath);
-
-    loadingTask.promise
-      .then((pdfDocument) => {
-        // Request a first page
-        return pdfDocument.getPage(1).then(function (pdfPage) {
-          // Display page on the existing canvas with 100% scale.
-          const viewport = pdfPage.getViewport({ scale: 1.0 });
-          const canvas = document.getElementById('canvas');
-          canvas.width = viewport.width;
-          canvas.height = viewport.height;
-          const ctx = canvas.getContext('2d');
-          const renderTask = pdfPage.render({
-            canvasContext: ctx,
-            viewport: viewport,
-          });
-          return renderTask.promise;
-        });
-      })
-      .catch((reason) => {
-        console.error('Error: ' + reason);
-      });
-  }
+export default class PDFViewer extends React.Component<IPDFViewerProps> {
+  componentDidMount() {}
 
   render(): React.ReactElement {
-    return <canvas id="canvas" />;
+    return (
+      <Document file={pdfPath}>
+        <Page pageNumber={3} />
+      </Document>
+    );
   }
 }
