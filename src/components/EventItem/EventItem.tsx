1257bebfefe6e8c80b4d9acc673f19d998a40827
Konstantin Kurganov <kurganovk@gmail.com>
2020-10-26 11:06:06 +0300

    Event items prototype

---
+++ src/components/EventItem/EventItem.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { ipcRenderer } from 'electron';
+import { connect } from 'react-redux';
+import { StoreType } from '../../reduxStore/store';
+// import { actions as projectFileActions } from '../../reduxStore/projectFileSlice';
+// import { actions as appStateActions } from '../../reduxStore/appStateSlice';
+import * as appConst from '../../types/textConstants';
+import { IEvent } from '../../types/event';
+
+export interface IEventItemProps {
+  event: IEvent;
+}
+export interface IEventItemState {}
+
+class EventItem extends React.Component<
+  StatePropsType & DispatchPropsType,
+  IEventItemState
+> {
+  constructor(props: StatePropsType & DispatchPropsType) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount(): void {
+    this.initListeners();
+  }
+
+  initListeners = (): void => {
+    // ipcRenderer.on(appConst.PDF_FILE_CONTENT_RESPONSE, (event, data) => {
+    //   this.setState({ pdfData: { data } });
+    // });
+  };
+
+  render(): React.ReactElement {
+    const { event } = this.props;
+    console.log('event', event);
+    return (
+      <div className="event-item">
+        <div className="event-title">{event.title}</div>
+        <div className="event-description">{event.description}</div>
+        <div className="event-date">{event?.date?.toString()}</div>
+        <div className="event-pdf-files">
+          {event.files.map((file, index) => {
+            return (
+              <div className="event-pdf-file" key={'event-key' + index}>
+                {file.path}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  // setFile: projectFileActions.setFile,
+  // setAppState: appStateActions.setAppState,
+};
+
+const mapStateToProps = (state: StoreType, ownProps: IEventItemProps) => {
+  return {
+    event: ownProps.event,
+    // currentAppState: state.appState.current,
+    // projectFileContent: state.projectFile.content,
+  };
+};
+
+type StatePropsType = ReturnType<typeof mapStateToProps>;
+type DispatchPropsType = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventItem);
--- src/components/EventsArea/EventsArea.tsx
+++ src/components/EventsArea/EventsArea.tsx
@@ -1,22 +1,48 @@
 import './events-area.scss';
 import * as React from 'react';
-// import Counter from '../Counter/Counter';
+import { ipcRenderer } from 'electron';
+import { connect } from 'react-redux';
+import { StoreType } from '../../reduxStore/store';
+import { actions as projectFileActions } from '../../reduxStore/projectFileSlice';
+import { actions as appStateActions } from '../../reduxStore/appStateSlice';
+import * as appConst from '../../types/textConstants';
+import { IEvent } from '../../types/event';
+import EventItem from '../EventItem/EventItem';
 
 export interface IEventsAreaProps {}
 export interface IEventsAreaState {}
 
-export default class EventsArea extends React.Component<
-  IEventsAreaProps,
+class EventsArea extends React.Component<
+  StatePropsType & DispatchPropsType,
   IEventsAreaState
 > {
   componentDidMount() {}
 
   render(): React.ReactElement {
+    const { projectFileContent } = this.props;
     return (
       <div className="events-area">
-        Events area
-        {/* <Counter offset={100} /> */}
+        {projectFileContent?.events?.map((event, index) => {
+          return <EventItem event={event} key={'event-item-key' + index} />;
+        })}
       </div>
     );
   }
 }
+
+const mapDispatchToProps = {
+  // setFile: projectFileActions.setFile,
+  // setAppState: appStateActions.setAppState,
+};
+
+const mapStateToProps = (state: StoreType, ownProps: IEventsAreaProps) => {
+  return {
+    // currentAppState: state.appState.current,
+    projectFileContent: state.projectFile.content,
+  };
+};
+
+type StatePropsType = ReturnType<typeof mapStateToProps>;
+type DispatchPropsType = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsArea);
